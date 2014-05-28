1e37dad25d09c70397f9984d3e3f4b0672d68888
Clément <clement.eberhardt@ensam.eu>
2014-05-28 15:55:43 +0200

    Merge branch 'developpement' of https://bitbucket.org/pjmsph/pjm-ensam-sph into developpement
    Conflicts:
    	app/solver/model/state.py

--- test/integration/solver.py
+++ test/integration/solver.py
@@ -22,15 +22,39 @@ import app.solver.model.fluid as m_fluid
 import app.solver.model.hash_table as m_hash
 import app.solver.model.vector as m_vec
 import app.solver.model.kernel as m_kern
+import app.solver.model.solver as m_solver
+import random
+import pytest
 
+
+def create_part_fluid(self):
+    base = m_solver.BaseSolver(10, 0.1)
+    fl = m_fluid.Fluid(1, 1, 1, 1, 1, 1, 1)
+
+    solve = m_solver.SphSolver(base)
+    vec = m_vec.Vector([random.randint(0, 1000), random.randint(0, 1000), random.randint(0, 1000)])
+    solve.create_active_particle(vec, fl, 0.001)
+    print(solve.particles)
+
+
+class TestCreatePart:
+
+    def test_call_raise_attribute_error_0(self):
+        with pytest.raises(TypeError):
+            create_part_fluid()
+
+
+
+
+'''
 kern = m_kern.SpikyKernel(10)
 V = ForcePressure("sals", kern, m_vec.Vector([1, 0, 2]))
 
 pt1 = m_vec.Vector([100, 200, 300])
 pt2 = m_vec.Vector([102, 201, 300])
-fl = m_fluid.Fluid(1, 1, 1, 1, 1, 1, 1)
+
 hashing = m_hash.Hash(1, 1000)
 A = m_part.ActiveParticule(hashing, pt1, 1, fl)
 B = m_part.ActiveParticule(hashing, pt2, 1, fl)
 t = SurfaceTension("Tension de surface", kern, m_vec.Vector([0, 0, 0]))
-print(t(A, A.neighbour(5, True)))
+print(t(A, A.neighbour(5, True)))'''
