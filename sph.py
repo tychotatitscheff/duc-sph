57cb0dabbeb7172289c78ed166d319bccd32a63d
Théo <tho.subtil@gadz.org>
2014-06-02 10:24:40 +0200

    sauvegarde en cours

--- sph.py
+++ sph.py
@@ -20,38 +20,38 @@ __status__ = "Production"
 
 
 if __name__ == "__main__":
-    import app.solver.model.fluid
+    import app.solver.model.fluid as s_flu
     import app.gui
     import app.save.mongo as mongo
-    import app.save.json as json
+    import app.save.json as d_json
 
     db = mongo.create_database('SPH')
     # pas encore testé
     # L'utilisateur veut créer un nouveau projet
-    nom_project = ""  # come from gui
+    nom_project = "jean-claude"  # come from gui
     project = mongo.create_collection(nom_project, db)
-    nombre_d_itération = ""  # come from gui
+    nombre_d_itération = 8  # come from gui
     for i in range(0, nombre_d_itération):
         state = mongo.create_collection('State ' + str(i), project)
         group_fluid = mongo.create_collection('fluid', state)
-        number_of_fluid = ""  # come from gui
-        for j in range(1, number_of_fluid):
-            fluid = ""  # come from gui
-            pickled_fluid = json.encode(fluid[j])
+        number_of_fluid = 1  # come from gui
+        for j in range(0, number_of_fluid):
+            fluid = [s_flu.Fluid(1, 2, 3, 4, 5, 6, 7)]  # come from gui
+            pickled_fluid = d_json.json_encode(fluid[j])
             mongo.create_document(pickled_fluid, group_fluid)
         group_particle = mongo.create_collection('particle', state)
-        number_of_particle = ""  # come from gui
-        for j in range(1, number_of_particle):
-            particle = ""  # come from gui
-            pickled_particle = json.encode(particle[j])
+        number_of_particle = 4  # come from gui
+        for j in range(0, number_of_particle):
+            particle = ["Classe particle", "a", "e", "32"]  # come from gui
+            pickled_particle = d_json.json_encode(particle[j])
             mongo.create_document(pickled_particle, group_particle)
-        acc_structural = ""  # come from gui²
-        pickled_acc_structural = json.encode(acc_structural)
+        acc_structural = "classe structure"  # come from gui²
+        pickled_acc_structural = d_json.json_encode(acc_structural)
         mongo.create_document(pickled_acc_structural, state)
-    geometrie = ""  # come from gui
-    pickled_geometrie = json.json_encode(geometrie)
+    geometrie = "classe geometrie"  # come from gui
+    pickled_geometrie = d_json.json_encode(geometrie)
     mongo.create_document(pickled_geometrie, project)
-    meta_data = ""  # come from gui²
-    pickled_meta_data = json.json_encode(meta_data)
+    meta_data = "classe meta data"  # come from gui²
+    pickled_meta_data = d_json.json_encode(meta_data)
     mongo.create_document(pickled_meta_data, project)
-            # en cours de production
+
