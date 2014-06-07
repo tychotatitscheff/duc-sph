e36e00ee619994f68f8a3655732bbe05bd756699
tycho.tatitscheff <tycho.tatitscheff@gadz.org>
2014-06-07 16:08:58 +0200

    GL working +  QtStrechFactor + delete tries + license

--- app/gui/widgets/glViewer.py
+++ app/gui/widgets/glViewer.py
@@ -1,15 +1,33 @@
 __author__ = 'salas'
 
-import sys
-import math
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = "Clément Eberhardt," \
+             "Clément Léost," \
+             "Benoit Picq," \
+             "Théo Subtil" \
+             " and Tycho Tatitscheff"
+__copyright__ = "Copyright 2014, DucSph"
+__credits__ = ["Clément Eberhardt",
+               "Clément Léost",
+               "Benoit Picq",
+               "Théo Subtil",
+               "Tycho Tatitscheff"]
+__license__ = "MIT"
+__version__ = "1.0.1"
+__maintainer__ = "Tycho Tatitscheff"
+__email__ = "tycho.tatitscheff@ensam.eu"
+__status__ = "Production"
 import numpy as np
 
 from PyQt4 import QtCore, QtGui, QtOpenGL
 from OpenGL import GL
-from OpenGL.arrays import vbo
-from OpenGL.GL import shaders
+from OpenGL import GLU
+
 
 import app.gui.figure.sphere as f_sphere
+
 
 class GLWidget(QtOpenGL.QGLWidget):
     GL_MULTISAMPLE = 0x809D
@@ -23,13 +41,18 @@ class GLWidget(QtOpenGL.QGLWidget):
         self.x_rot = 0
         self.y_rot = 0
         self.z_rot = 0
+        self.x_trans = 0.5
+        self.y_trans = 0.5
+        self.scale = .5
 
         self.last_pos = QtCore.QPoint()
 
         self.troll_tech_green = QtGui.QColor.fromCmykF(0.20, 0.4, 1.0, 0.0)
         self.troll_tech_purple = QtGui.QColor.fromCmykF(0.39, 0.39, 0.0, 0.0)
 
-        self.draw = ()
+        self.vertices = None
+        self.index = None
+        self.color = None
 
     def get_rotation_x(self):
         return self.x_rot
@@ -67,8 +90,27 @@ class GLWidget(QtOpenGL.QGLWidget):
             self.emit(QtCore.SIGNAL("zRotationChanged(int)"), angle)
             self.updateGL()
 
+    def set_scale(self, scale):
+        if scale != self.scale:
+            self.scale = abs(scale)
+            self.emit(QtCore.SIGNAL("ScaleChanged(int)"), scale)
+            self.updateGL()
+
+    def set_trans_x(self, trans):
+        if trans != self.x_trans:
+            self.x_trans = trans
+            self.emit(QtCore.SIGNAL("xTransChanged(int)"), trans)
+            self.updateGL()
+
+    def set_trans_y(self, trans):
+        if trans != self.y_trans:
+            self.y_trans = trans
+            self.emit(QtCore.SIGNAL("yTransChanged(int)"), trans)
+            self.updateGL()
+
     def initializeGL(self):
         self.qglClearColor(self.troll_tech_purple.darker())
+        self.make_object()
         GL.glShadeModel(GL.GL_FLAT)
         GL.glEnable(GL.GL_DEPTH_TEST)
         GL.glEnable(GL.GL_CULL_FACE)
@@ -85,30 +127,35 @@ class GLWidget(QtOpenGL.QGLWidget):
 
     def paintGL(self):
         GL.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT)
-        self.draw = self.make_object()
+
         GL.glLoadIdentity()
-        GL.glTranslated(0.0, 0.0, -10.0)
+        GL.glTranslate(self.x_trans, self.y_trans, -50)
         GL.glRotated(self.x_rot / 16.0, 1.0, 0.0, 0.0)
         GL.glRotated(self.y_rot / 16.0, 0.0, 1.0, 0.0)
         GL.glRotated(self.z_rot / 16.0, 0.0, 0.0, 1.0)
+        GL.glScale(self.scale, self.scale, self.scale)
         GL.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT)
 
-        self.draw[1].bind()
-
-        self.draw[0].bind()
-
-        GL.glEnableVertexAttribArray(0)
-        GL.glVertexAttribPointer(0, 3, GL.GL_FLOAT, False, 0, None)
-
-        GL.glDrawElements(GL.GL_TRIANGLES, 3, GL.GL_UNSIGNED_INT, None)
+        GL.glEnableClientState(GL.GL_VERTEX_ARRAY)
+        GL.glEnableClientState(GL.GL_COLOR_ARRAY)
+        GL.glVertexPointerf(self.vertices)
+        GL.glColorPointerf(self.color)
+        GL.glDrawElementsui(GL.GL_QUADS, self.index.tolist())
 
     def resizeGL(self, width, height):
         side = min(width, height)
-        GL.glViewport(int((width - side) / 2), int((height - side) / 2), side, side)
-
+        #GL.glViewport(int((width - side) / 2), int((height - side) / 2), side, side)
+        #
+        # GL.glMatrixMode(GL.GL_PROJECTION)
+        # GL.glLoadIdentity()
+        #GL.glOrtho(-0.5, +0.5, -0.5, +0.5, 4.0, 15.0)
+        # GL.glMatrixMode(GL.GL_MODELVIEW)
+        GL.glViewport(0, 0, width, height)
         GL.glMatrixMode(GL.GL_PROJECTION)
         GL.glLoadIdentity()
-        GL.glOrtho(-0.5, +0.5, -0.5, +0.5, 4.0, 15.0)
+        aspect = width / float(height)
+
+        GLU.gluPerspective(45.0, aspect, 1.0, 100.0)
         GL.glMatrixMode(GL.GL_MODELVIEW)
 
     def mousePressEvent(self, event):
@@ -118,30 +165,39 @@ class GLWidget(QtOpenGL.QGLWidget):
         dx = event.x() - self.last_pos.x()
         dy = event.y() - self.last_pos.y()
 
-        if event.buttons() & QtCore.Qt.LeftButton:
-            self.set_rotation_x(self.x_rot + 8 * dy)
-            self.set_rotation_y(self.y_rot + 8 * dx)
-        elif event.buttons() & QtCore.Qt.RightButton:
+        if event.buttons() & QtCore.Qt.MiddleButton:
+            self.set_trans_x(self.x_trans + .1 * dx)
+            self.set_trans_y(self.y_trans - .1 * dy)
+        elif event.buttons() & QtCore.Qt.LeftButton:
             self.set_rotation_x(self.x_rot + 8 * dy)
             self.set_rotation_z(self.z_rot + 8 * dx)
+        elif event.buttons() & QtCore.Qt.RightButton:
+            self.set_scale(self.scale + .05 * dy)
 
         self.last_pos = QtCore.QPoint(event.pos())
 
-    @staticmethod
-    def make_object():
-        vertices = None
-        index = None
-        for i in range(1, 3):
-            if vertices is None:
-                vertices = f_sphere.add_sphere_vertices(np.array([i, i, i]), 1)
-                index = f_sphere.add_sphere_index()
+    def wheelEvent(self, event):
+        delta = event.delta()
+        self.set_scale(self.scale + delta / 180)
+
+    def make_object(self):
+        col = [0.8, 0.8, 0.8]
+        for j in range(1, 10):
+            v = f_sphere.add_sphere_vertices(np.array([2.4 * j, 0, 0]), 1)
+            if self.vertices is None:
+                i = f_sphere.add_sphere_index()
+                c = [col for x in i]
+                self.vertices = v
+                self.index = i
+                self.color = c
             else:
-                idx_max = np.amax(index)
-                vertices = np.append(vertices, f_sphere.add_sphere_vertices(np.array([i, i, i]), 1))
-                index = np.append(index, f_sphere.add_sphere_index(idx_max))
-        vertexPositions = vbo.VBO(vertices)
-        indexPositions = vbo.VBO(index, target=GL.GL_ELEMENT_ARRAY_BUFFER)
-        return vertexPositions, indexPositions
+                idx_max = np.amax(self.index) + 1
+                i = f_sphere.add_sphere_index(idx_max)
+                c = [col for x in i]
+                self.vertices = np.append(self.vertices, v, axis=0)
+                self.index = np.append(self.index, i)
+                self.color.extend(c)
+
 
     @staticmethod
     def normalize_angle(angle):
--- app/gui/windows_main.py
+++ app/gui/windows_main.py
@@ -1,4 +1,25 @@
 __author__ = 'salas'
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = "Clément Eberhardt," \
+             "Clément Léost," \
+             "Benoit Picq," \
+             "Théo Subtil" \
+             " and Tycho Tatitscheff"
+__copyright__ = "Copyright 2014, DucSph"
+__credits__ = ["Clément Eberhardt",
+               "Clément Léost",
+               "Benoit Picq",
+               "Théo Subtil",
+               "Tycho Tatitscheff"]
+__license__ = "MIT"
+__version__ = "1.0.1"
+__maintainer__ = "Tycho Tatitscheff"
+__email__ = "tycho.tatitscheff@ensam.eu"
+__status__ = "Production"
+
 import sys
 
 import qdarkstyle
@@ -10,6 +31,7 @@ import app.gui.widgets.glViewer as g_gl
 class Window(QtGui.QMainWindow):
     def __init__(self, parent=None):
         QtGui.QWidget.__init__(self, parent)
+        self.showMaximized()
         exit_action = QtGui.QAction(QtGui.QIcon('exit.png'), '&Exit', self)
         exit_action.setShortcut('Ctrl+Q')
         exit_action.setStatusTip('Exit application')
@@ -22,6 +44,7 @@ class Window(QtGui.QMainWindow):
         menu_bar = self.menuBar()
         file_menu = menu_bar.addMenu('&File')
         file_menu.addAction(exit_action)
+        simulation_menu = menu_bar.addMenu('Simulation')
 
         self.main_layout = QtGui.QVBoxLayout(self.central_widget)
         self.splitter_v = QtGui.QSplitter(QtCore.Qt.Vertical)
@@ -32,11 +55,17 @@ class Window(QtGui.QMainWindow):
         self.node_editor = QtGui.QFrame()
         self.splitter_v.addWidget(self.node_editor)
 
+        self.splitter_v.setStretchFactor(0, 2)
+        self.splitter_v.setStretchFactor(1, 1)
+
         self.splitter_h = QtGui.QSplitter(QtCore.Qt.Horizontal)
         self.splitter_h.addWidget(self.splitter_v)
 
         self.properties = QtGui.QFrame()
         self.splitter_h.addWidget(self.properties)
+
+        self.splitter_h.setStretchFactor(0, 7)
+        self.splitter_h.setStretchFactor(1, 1)
 
         self.main_layout.addWidget(self.splitter_h)
 
